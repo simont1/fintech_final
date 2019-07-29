f9ac8713c37fc298e126f4defd91183459e866e8
simont1 <88simont@gmail.com>
2019-07-29 14:00:41 +0000

    csv added

--- app/back.py
+++ app/back.py
@@ -1,21 +1,34 @@
 #functions go here
+import csv
 
-stocks =[
-    {"symbol": "GOOGL", "price": 1253.22},    
-    {"symbol": "MSFT", "price": 141.53}
-    # {"symbol": "", "price": }
-    # {"symbol": "", "price": }
-    # {"symbol": "", "price": }
+
+stocks = [
 ]
+
+
+with open('static/sp500.csv', newline='') as csvfile:
+     lines = csv.reader(csvfile, delimiter=',', quotechar='|')
+     for row in lines:
+        #  print(row[0] + ", " + row[1] + "'s closing price is " + row[2])
+         stocks.append({"symbol": row[0], "name": row[1], "close": float(row[2])})
+
+
+# stocks =[
+    # {"symbol": "GOOGL", "price": 1253.22},    
+    # {"symbol": "MSFT", "price": 141.53}
+    # {"symbol": "", "price": }
+    # {"symbol": "", "price": }
+    # {"symbol": "", "price": }
+# ]
 
 
 def price_range(money):
     ret_arr = []
     for stock in stocks:
-        if money > stock["price"]:
-            shares_available = money / stock["price"]
+        if money > stock["close"]:
+            shares_available = money / stock["close"]
             ret_arr.append({stock["symbol"]: str(int(shares_available)) + " shares purchasable"})
             # ret_arr.append(stock["symbol"] + " " + str(int(shares_available)) + " shares purchasable")
     return ret_arr
             
-price_range(5000)
+print(price_range(5000))
